d34ff3fa21f30feb3c99ff0b923df7f922848007
Nathaniel <restofnate@gmail.com>
2026-02-18 21:59:20 -0500

    Added constants and configs for Robot (indexing, conveor belt, shooter, hood)

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
@@ -24,7 +24,7 @@ public class FlywheelConstants {
   // Conveyor Constants
   // -----------
   public static final FlywheelHardwareConfig CONVEYOR_CONFIG =
-      new FlywheelHardwareConfig(new int[] {}, new boolean[] {false}, 1, 0, "");
+      new FlywheelHardwareConfig(new int[] {46}, new boolean[] {false}, 1, 40, "");
   public static final FlywheelGains CONVEYOR_GAINS =
       new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
 
@@ -32,7 +32,34 @@ public class FlywheelConstants {
   // Intake Constants
   // -----------
   public static final FlywheelHardwareConfig INTAKE_ROLLER_CONFIG =
-      new FlywheelHardwareConfig(new int[] {}, new boolean[] {false}, 1, 0, "");
+      new FlywheelHardwareConfig(new int[] {45}, new boolean[] {false}, 1, 40, "");
   public static final FlywheelGains INTAKE_ROLLER_GAINS =
       new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
+
+
+ //------------
+ // Indexer Constants
+ //------------
+public static final FlywheelHardwareConfig TOP_INDEXER_ROLLER_CONFIG = 
+    new FlywheelHardwareConfig(new int[] {43}, new boolean[] {false}, 1, 40, "");
+public static final FlywheelHardwareConfig BOTTOM_INDEXER_ROLLER_CONFIG = 
+    new FlywheelHardwareConfig(new int[] {44}, new boolean[] {false}, 1, 40, "");
+public static final FlywheelGains INDEXER_ROLLER_GAINS = 
+    new FlywheelGains(0, 0, 0, 0, 0, 0, 0, 0);
+
+
+
+
+//------------
+ // Shooter Constants
+ //------------
+public static final FlywheelHardwareConfig FLYWHEEL_ROLLER_CONFIG = 
+    new FlywheelHardwareConfig(new int[] {41,42}, new boolean[] {false, true}, 1, 40, "");
+public static final FlywheelGains FLYWHEEL_ROLLER_GAINS = 
+    new FlywheelGains(0.2, 0.0, 0.0, 0.0, 0.065, 0.0, 1.0, 1.0);
+
+
+
+
+
 }
