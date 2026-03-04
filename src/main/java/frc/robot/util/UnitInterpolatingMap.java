f195bdbd056e4809860a39056e41cd7f94209617
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-03-04 16:10:17 -0500

    Removed License after modifyin

--- src/main/java/frc/robot/util/UnitInterpolatingMap.java
+++ src/main/java/frc/robot/util/UnitInterpolatingMap.java
@@ -1,15 +1,3 @@
-/*
-* ALOTOBOTS - FRC Team 5152
-  https://github.com/5152Alotobots
-* Copyright (C) 2026 ALOTOBOTS
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Source code must be publicly available on GitHub or an alternative web accessible site
-*/
 package frc.robot.util;
 
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
