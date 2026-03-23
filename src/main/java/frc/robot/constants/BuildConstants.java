27556fd7079f85b31ab49b7dd4d934dd3eb69a8c
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-03-23 15:24:30 -0400

    Refined first trajectory

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 305;
-	public static final String GIT_SHA = "c2122d6f1d45c396ebf130b8d2f11d85c9af1abf";
-	public static final String GIT_DATE = "2026-03-23 08:54:10 EDT";
+	public static final int GIT_REVISION = 308;
+	public static final String GIT_SHA = "dc9720b06425328b81ea2dd6746a756501de4c8b";
+	public static final String GIT_DATE = "2026-03-23 12:38:30 EDT";
 	public static final String GIT_BRANCH = "Choreo";
-	public static final String BUILD_DATE = "2026-03-23 11:26:16 EDT";
-	public static final long BUILD_UNIX_TIME = 1774279576939L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2026-03-23 13:52:50 EDT";
+	public static final long BUILD_UNIX_TIME = 1774288370785L;
+	public static final int DIRTY = 0;
 
 	private BuildConstants() {
 	}
