bc2fb559ab520481aa7782d38322b19ac663f421
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-03-23 11:30:03 -0400

    New build

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 304;
-	public static final String GIT_SHA = "3842d37690597043f8e14393fd1730e7d7a52d71";
-	public static final String GIT_DATE = "2026-03-22 21:27:02 EDT";
-	public static final String GIT_BRANCH = "ctre-ifying_drive_commands";
-	public static final String BUILD_DATE = "2026-03-23 08:46:13 EDT";
-	public static final long BUILD_UNIX_TIME = 1774269973583L;
+	public static final int GIT_REVISION = 305;
+	public static final String GIT_SHA = "c2122d6f1d45c396ebf130b8d2f11d85c9af1abf";
+	public static final String GIT_DATE = "2026-03-23 08:54:10 EDT";
+	public static final String GIT_BRANCH = "Choreo";
+	public static final String BUILD_DATE = "2026-03-23 11:26:16 EDT";
+	public static final long BUILD_UNIX_TIME = 1774279576939L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
