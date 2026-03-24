ad4ff5cbc8e6b00d561069e6f2658040b16a2ed4
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-03-23 23:23:03 -0400

    Reimlemented sim intake activation

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 305;
-	public static final String GIT_SHA = "79f23ffd149ebbdcacada82b77896ab252e19d7c";
-	public static final String GIT_DATE = "2026-03-23 18:52:41 EDT";
+	public static final int GIT_REVISION = 306;
+	public static final String GIT_SHA = "bbf547af0a14a9263a566161c91e7184becfba61";
+	public static final String GIT_DATE = "2026-03-23 22:50:31 EDT";
 	public static final String GIT_BRANCH = "maplesimPlusCTRE";
-	public static final String BUILD_DATE = "2026-03-23 22:29:41 EDT";
-	public static final long BUILD_UNIX_TIME = 1774319381118L;
+	public static final String BUILD_DATE = "2026-03-23 23:21:53 EDT";
+	public static final long BUILD_UNIX_TIME = 1774322513660L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
