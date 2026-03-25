4475e1079eab70b7c68471dfa1f7da976e0e84bf
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-03-24 20:27:01 -0400

    Changed P term down on swerve module

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 303;
-	public static final String GIT_SHA = "8e3408d8a6880b6184ed22b886f20ab7e3f33c6f";
-	public static final String GIT_DATE = "2026-03-21 21:22:02 EDT";
-	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-03-22 21:21:39 EDT";
-	public static final long BUILD_UNIX_TIME = 1774228899228L;
+	public static final int GIT_REVISION = 306;
+	public static final String GIT_SHA = "a48cb9f1be56fd1462cb833a6c69bd70faf4c24f";
+	public static final String GIT_DATE = "2026-03-24 16:40:30 EDT";
+	public static final String GIT_BRANCH = "recalcDrivetrainGains";
+	public static final String BUILD_DATE = "2026-03-24 20:25:43 EDT";
+	public static final long BUILD_UNIX_TIME = 1774398343101L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
--- src/main/java/frc/robot/constants/drive/TunerConstants.java
+++ src/main/java/frc/robot/constants/drive/TunerConstants.java
@@ -24,7 +24,7 @@ public class TunerConstants {
 	// The steer motor uses any SwerveModule.SteerRequestType control request with
 	// the
 	// output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
-	private static final Slot0Configs steerGains = new Slot0Configs().withKP(200).withKI(0).withKD(1).withKS(0.1)
+	private static final Slot0Configs steerGains = new Slot0Configs().withKP(100).withKI(0).withKD(1).withKS(0.1)
 			.withKV(2.0).withKA(0.01).withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
 	// When using closed-loop control, the drive motor uses the control
 	// output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
