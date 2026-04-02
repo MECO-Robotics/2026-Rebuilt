a66d16c9ce630101cba8c230ec92a299de0037ac
Nathaniel Foster <79800052+nfoster262@users.noreply.github.com>
2026-04-01 22:07:25 -0400

    Changed some auto commands and added test auto

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 304;
-	public static final String GIT_SHA = "3842d37690597043f8e14393fd1730e7d7a52d71";
-	public static final String GIT_DATE = "2026-03-22 21:27:02 EDT";
+	public static final int GIT_REVISION = 305;
+	public static final String GIT_SHA = "a1d5a68f291d3a5ab2aa042baefea121120aaa48";
+	public static final String GIT_DATE = "2026-03-31 20:56:46 EDT";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-03-31 20:20:11 EDT";
-	public static final long BUILD_UNIX_TIME = 1775002811431L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2026-04-01 21:49:52 EDT";
+	public static final long BUILD_UNIX_TIME = 1775094592766L;
+	public static final int DIRTY = 0;
 
 	private BuildConstants() {
 	}
