a516c663344e6e281b4d5309567b600ab3cc0037
NotTheRealMozart <nottherealmozart@gmail.com>
2026-04-02 09:04:41 -0400

    nate did this

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 120;
+  public static final String GIT_SHA = "4a248a5a0e168c98a8a2b1ba45b71e4c42618659";
+  public static final String GIT_DATE = "2026-01-28 19:05:45 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-04 18:47:08 EST";
+  public static final long BUILD_UNIX_TIME = 1770248828595L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
