7cc969fc7a6a42442be67c59e1158e51c6f17573
Nathaniel Foster <79800052+nfoster262@users.noreply.github.com>
2026-04-06 18:27:11 -0400

    Right Double created

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 373;
-	public static final String GIT_SHA = "5673352652a7b80ebf5551386cec5e081ff56919";
-	public static final String GIT_DATE = "2026-04-05 18:35:12 EDT";
-	public static final String GIT_BRANCH = "Pathplanner_Editing";
-	public static final String BUILD_DATE = "2026-04-05 20:50:50 EDT";
-	public static final long BUILD_UNIX_TIME = 1775436650198L;
-	public static final int DIRTY = 0;
+	public static final int GIT_REVISION = 384;
+	public static final String GIT_SHA = "b23f9ee9f9d71f1913b4a78a620be02c188a2c26";
+	public static final String GIT_DATE = "2026-04-06 07:03:31 EDT";
+	public static final String GIT_BRANCH = "Nate-dev";
+	public static final String BUILD_DATE = "2026-04-06 16:17:46 EDT";
+	public static final long BUILD_UNIX_TIME = 1775506666490L;
+	public static final int DIRTY = 1;
 
 	private BuildConstants() {
 	}
