0e0e4f061333fb5f406b98664a8f50ea9461ead2
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-04-12 14:17:26 -0400

    Tune shooter velocity map

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 406;
-	public static final String GIT_SHA = "0cff6e8019605b0b7554685f2318fc57dd1b11d3";
-	public static final String GIT_DATE = "2026-04-11 19:08:52 EDT";
+	public static final int GIT_REVISION = 407;
+	public static final String GIT_SHA = "d3c1895ec66ac3595e4d09b776afbae4a1679d62";
+	public static final String GIT_DATE = "2026-04-11 20:21:01 EDT";
 	public static final String GIT_BRANCH = "fix/position-joint-motion-magic";
-	public static final String BUILD_DATE = "2026-04-11 20:20:32 EDT";
-	public static final long BUILD_UNIX_TIME = 1775953232619L;
+	public static final String BUILD_DATE = "2026-04-11 20:27:04 EDT";
+	public static final long BUILD_UNIX_TIME = 1775953624480L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
