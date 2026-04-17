a13172467fd3ecdb7ef6f8395ecbd81b08b0a47b
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-04-17 09:55:52 -0400

    Update at 'Fri Apr 17 09:55:52 EDT 2026'

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 418;
-	public static final String GIT_SHA = "81212363e13d511cc78650bdf6998ae2cda90da6";
-	public static final String GIT_DATE = "2026-04-16 20:33:02 EDT";
-	public static final String GIT_BRANCH = "feature/limelight-megatag2-single-tag";
-	public static final String BUILD_DATE = "2026-04-16 20:38:28 EDT";
-	public static final long BUILD_UNIX_TIME = 1776386308887L;
-	public static final int DIRTY = 1;
+	public static final int GIT_REVISION = 427;
+	public static final String GIT_SHA = "bd49d21c7e90e58b1e918d678fb86c3e8a57c981";
+	public static final String GIT_DATE = "2026-04-17 09:48:49 EDT";
+	public static final String GIT_BRANCH = "event_tallahassee_regional";
+	public static final String BUILD_DATE = "2026-04-17 09:55:00 EDT";
+	public static final long BUILD_UNIX_TIME = 1776434100269L;
+	public static final int DIRTY = 0;
 
 	private BuildConstants() {
 	}
