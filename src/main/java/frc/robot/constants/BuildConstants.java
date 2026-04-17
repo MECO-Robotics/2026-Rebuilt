066fea3f40ce6eb010bfd0851b55c181ac775325
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-04-17 11:43:30 -0400

    More auto tweaks

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 428;
-	public static final String GIT_SHA = "a13172467fd3ecdb7ef6f8395ecbd81b08b0a47b";
-	public static final String GIT_DATE = "2026-04-17 09:55:52 EDT";
+	public static final int GIT_REVISION = 429;
+	public static final String GIT_SHA = "00bd9f9063ff2f4b48c57cee3e88410411c34ae9";
+	public static final String GIT_DATE = "2026-04-17 09:58:19 EDT";
 	public static final String GIT_BRANCH = "event_tallahassee_regional";
-	public static final String BUILD_DATE = "2026-04-17 09:55:53 EDT";
-	public static final long BUILD_UNIX_TIME = 1776434153454L;
+	public static final String BUILD_DATE = "2026-04-17 09:58:20 EDT";
+	public static final long BUILD_UNIX_TIME = 1776434300095L;
 	public static final int DIRTY = 0;
 
 	private BuildConstants() {
