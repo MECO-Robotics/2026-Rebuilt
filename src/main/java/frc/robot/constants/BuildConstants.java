b809c0410ad549706cecc763551b96044872df9d
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-04-17 08:47:06 -0400

    Moses inital auto & tweaks

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 418;
-	public static final String GIT_SHA = "81212363e13d511cc78650bdf6998ae2cda90da6";
-	public static final String GIT_DATE = "2026-04-16 20:33:02 EDT";
-	public static final String GIT_BRANCH = "feature/limelight-megatag2-single-tag";
-	public static final String BUILD_DATE = "2026-04-16 20:38:28 EDT";
-	public static final long BUILD_UNIX_TIME = 1776386308887L;
+	public static final int GIT_REVISION = 420;
+	public static final String GIT_SHA = "c40c88883257f74673d1deb225a5a7ce1c28eede";
+	public static final String GIT_DATE = "2026-04-16 23:53:42 EDT";
+	public static final String GIT_BRANCH = "Tally_Qual_Autos";
+	public static final String BUILD_DATE = "2026-04-17 08:44:00 EDT";
+	public static final long BUILD_UNIX_TIME = 1776429840145L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
