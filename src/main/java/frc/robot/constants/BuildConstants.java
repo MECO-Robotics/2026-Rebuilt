4e504f5a66d5fb4c976f55e0e499d7f4b427f183
Amadeus Jackson <nottherealmozart@gmail.com>
2026-06-27 08:28:30 -0400

    Fix drivetrain build syntax

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2026-Rebuilt";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 468;
-	public static final String GIT_SHA = "8fbda57c40a2482efb9c2107d2b187752939435b";
-	public static final String GIT_DATE = "2026-05-18 19:37:15 EDT";
+	public static final int GIT_REVISION = 469;
+	public static final String GIT_SHA = "e988d6f0c047f79c7cec63854c6cc82df042012b";
+	public static final String GIT_DATE = "2026-06-27 08:15:34 EDT";
 	public static final String GIT_BRANCH = "feature/Amadeus-auto-driving";
-	public static final String BUILD_DATE = "2026-06-27 08:14:14 EDT";
-	public static final long BUILD_UNIX_TIME = 1782562454496L;
+	public static final String BUILD_DATE = "2026-06-27 08:18:28 EDT";
+	public static final long BUILD_UNIX_TIME = 1782562708847L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
